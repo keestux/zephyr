6dbf303ad7c50543ba4ada0765c694773e391bfa
Kees Bakker <kees@ijzerbout.nl>
2019-11-05 21:02:51 +0100

    fixup! Add support for board Sodaq Autonomo

--- boards/arm/sodaq_autonomo/board.cmake
+++ boards/arm/sodaq_autonomo/board.cmake
@@ -1,4 +1,5 @@
 # Copyright (c) 2017 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+board_runner_args(bossac "--offset=0x2000")
+include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
