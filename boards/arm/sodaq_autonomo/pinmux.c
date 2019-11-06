2f8f8d44fede62b84e353436c7cf2498a3ec59d0
Kees Bakker <kees@ijzerbout.nl>
2019-11-06 21:36:21 +0100

    fixup! Add support for board Sodaq Autonomo

--- boards/arm/sodaq_autonomo/pinmux.c
+++ boards/arm/sodaq_autonomo/pinmux.c
@@ -9,21 +9,23 @@
 
 static int board_pinmux_init(struct device *dev)
 {
-	struct device *muxa = device_get_binding(DT_ATMEL_SAM0_PINMUX_PINMUX_A_LABEL);
-	struct device *muxb = device_get_binding(DT_ATMEL_SAM0_PINMUX_PINMUX_B_LABEL);
+	struct device *porta = device_get_binding(DT_ATMEL_SAM0_PINMUX_PINMUX_A_LABEL);
+	struct device *portb = device_get_binding(DT_ATMEL_SAM0_PINMUX_PINMUX_B_LABEL);
 
 	ARG_UNUSED(dev);
 
 #if DT_ATMEL_SAM0_UART_SERCOM_0_BASE_ADDRESS
-	/* SERCOM0 on RX=PA11, TX=PA10 */
-	pinmux_pin_set(muxa, 11, PINMUX_FUNC_C);
-	pinmux_pin_set(muxa, 10, PINMUX_FUNC_C);
+	/* SERCOM0 UART on RX=PA9, TX=PA10 */
+	pinmux_pin_set(porta, 9, PINMUX_FUNC_C);
+	pinmux_pin_set(porta, 10, PINMUX_FUNC_C);
 #endif
 
 #if DT_ATMEL_SAM0_UART_SERCOM_5_BASE_ADDRESS
-	/* SERCOM5 on RX=PB23, TX=PB22 */
-	pinmux_pin_set(muxb, 23, PINMUX_FUNC_D);
-	pinmux_pin_set(muxb, 22, PINMUX_FUNC_D);
+	/* SERCOM5 UART on RX=PB31, TX=PB30, RTS=PB22, CTS=PB23 */
+	pinmux_pin_set(portb, 31, PINMUX_FUNC_D);
+	pinmux_pin_set(portb, 30, PINMUX_FUNC_D);
+	pinmux_pin_set(portb, 22, PINMUX_FUNC_D);
+	pinmux_pin_set(portb, 23, PINMUX_FUNC_D);
 #endif
 
 #if DT_ATMEL_SAM0_UART_SERCOM_1_BASE_ADDRESS
@@ -39,11 +41,12 @@ static int board_pinmux_init(struct device *dev)
 #error Pin mapping is not configured
 #endif
 
-#if DT_ATMEL_SAM0_SPI_SERCOM_4_BASE_ADDRESS
-	/* SPI SERCOM4 on MISO=PA12/pad 0, MOSI=PB10/pad 2, SCK=PB11/pad 3 */
-	pinmux_pin_set(muxa, 12, PINMUX_FUNC_D);
-	pinmux_pin_set(muxb, 10, PINMUX_FUNC_D);
-	pinmux_pin_set(muxb, 11, PINMUX_FUNC_D);
+#if DT_ATMEL_SAM0_SPI_SERCOM_3_BASE_ADDRESS
+	/* SERCOM3 SPI on MISO=PA22/pad 0, MOSI=PA20/pad 2, SCK=PA21/pad 3, SS=PA23/pad 1 */
+	pinmux_pin_set(porta, 22, PINMUX_FUNC_C);
+	pinmux_pin_set(portb, 20, PINMUX_FUNC_D);
+	pinmux_pin_set(portb, 21, PINMUX_FUNC_D);
+	pinmux_pin_set(portb, 23, PINMUX_FUNC_C);
 #endif
 
 #if DT_ATMEL_SAM0_SPI_SERCOM_0_BASE_ADDRESS
@@ -55,7 +58,7 @@ static int board_pinmux_init(struct device *dev)
 #if DT_ATMEL_SAM0_SPI_SERCOM_2_BASE_ADDRESS
 #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_SPI_SERCOM_3_BASE_ADDRESS
+#if DT_ATMEL_SAM0_SPI_SERCOM_4_BASE_ADDRESS
 #error Pin mapping is not configured
 #endif
 #if DT_ATMEL_SAM0_SPI_SERCOM_5_BASE_ADDRESS
@@ -64,8 +67,8 @@ static int board_pinmux_init(struct device *dev)
 
 #ifdef CONFIG_USB_DC_SAM0
 	/* USB DP on PA25, USB DM on PA24 */
-	pinmux_pin_set(muxa, 25, PINMUX_FUNC_G);
-	pinmux_pin_set(muxa, 24, PINMUX_FUNC_G);
+	pinmux_pin_set(porta, 25, PINMUX_FUNC_G);
+	pinmux_pin_set(porta, 24, PINMUX_FUNC_G);
 #endif
 
 	return 0;
